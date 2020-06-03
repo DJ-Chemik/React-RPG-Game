43a029163d4d77ee2c2ad508bfe2072f4ed5bb25
DJ Chemik <dj-chemik@wp.pl>
2020-06-03 19:59:03 +0200

    Move files and create empty game project

---
+++ src/game/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './../css/Game.css';
+
+function Game() {
+  return (
+    <div className="Game">
+      Put here your game components
+    </div>
+  );
+}
+
+export default Game;
